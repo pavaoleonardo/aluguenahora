24843057c7341d8e03a3b71b8c53997628e38bf6
leonardo <pavaoleonardo@hotmail.com>
2026-01-10 16:54:50 +0100

    Complete CSS reset for site only

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/(site)/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  corePlugins: {
+    preflight: false, // Desabilita reset global do Tailwind
+  },
   theme: {
     extend: {},
   },
