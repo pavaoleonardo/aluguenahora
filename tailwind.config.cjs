d01428528f1345a0d720ee8c9fb49a50077866cb
leonardo <pavaoleonardo@hotmail.com>
2026-01-10 16:32:20 +0100

    Tailwind only on site with preflight disabled

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
     './src/app/(site)/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     extend: {},
   },
